9e39564845a6118578aa09583c5d50afeb3e2d00
Anup Aggarwal <anupaggarwal75@gmail.com>
2021-07-12 13:24:28 +0530

    Delete redundant lines

--- youtube_link.py
+++ youtube_link.py
@@ -3,13 +3,8 @@ import requests
 source = requests.get('https://coreyms.com/').text
 soup = BeautifulSoup(source, 'lxml')
 article = soup.find('article')
-
-# print(article.prettify())
 vid_src = article.find('iframe', class_='youtube-player')['src']
-# print(vid_src)
-
 vid_id = vid_src.split('/')[4]
 vid_id = vid_id.split('?')[0]
-# print(vid_id)
 yt_link = f'https://youtube.com/watch?v={vid_id}'
 print(yt_link)
